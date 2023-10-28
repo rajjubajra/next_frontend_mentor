ebda42d158fef49514046fd7020ebd2a3588d3cd
rajjubajra <rajjubajra@gmail.com>
2023-10-28 08:07:27 +0100

    Country Listing loading file

--- src/app/countries_api/page.js
+++ src/app/countries_api/page.js
@@ -2,7 +2,7 @@
 import {useState, useEffect} from 'react';
 import data from './data.json';
 import CountryListing from './CountyListing'; 
-import Loading from '../Loading';
+import Loading from '../loading';
 
 function CountriesApi() {
 
